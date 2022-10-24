8ddc28d9bad66d668c47db0ecb1d4f7f66fb40b8
safak.celik <safak44@live.de>
2022-10-24 19:10:04 +0200

    Added Example for ViewBinding

--- app/src/main/java/com/example/tradingapp/MainActivity.kt
+++ app/src/main/java/com/example/tradingapp/MainActivity.kt
@@ -2,42 +2,43 @@ package com.example.tradingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.tradingapp.R.drawable.ic_walk
 import com.example.tradingapp.databinding.AchievementCardFirstWeekBinding
 import com.example.tradingapp.databinding.ActivityMainBinding
 import com.example.tradingapp.databinding.InvestementFirstCardBinding
 import com.example.tradingapp.databinding.UserCardBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // Erstellen von ViewBinding Pbject
     private lateinit var binding: ActivityMainBinding
-    private lateinit var bindingCardUser: UserCardBinding
-    private lateinit var bindingAchievementCardFirstWeekBinding: AchievementCardFirstWeekBinding
-    private lateinit var bindingInvestementFirstCardBinding: InvestementFirstCardBinding
-    //vergesen
+    private lateinit var userCardBinding: UserCardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupBinding()
-        title = "__"
-        bindingCardUser.money.text = "400"
-        bindingAchievementCardFirstWeekBinding.number.text = "20"
-        bindingInvestementFirstCardBinding.root.setBackgroundColor(getColor(R.color.green))
-        bindingInvestementFirstCardBinding.restaurantWin.text= getString(R.string.restaurant_win)
-        bindingInvestementFirstCardBinding.imageRestaurant.setImageDrawable(getDrawable(ic_walk))
     }
 
     private fun setupBinding() {
-        //vergesen
-        binding = ActivityMainBinding.inflate(layoutInflater) //initializing the binding class
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         binding.run {
             this@MainActivity.apply {
-                bindingCardUser = viewUserCard
-                bindingAchievementCardFirstWeekBinding = achievementsFirstBlock
-                bindingInvestementFirstCardBinding = investementFirstBlock
+                /**
+                 * Wir weißen Objekt, was wir oben erstellt haben und ID von included Layout zu
+                 * userCardBinding = Objekt
+                 * viewUserCard = ID von Included Layout
+                 */
+                userCardBinding = includeUserCard
             }
         }
-
-
+        // Hier die Methode aufrufen
+        setupCardView()
     }
 
-
+    private fun setupCardView() {
+        userCardBinding.apply {
+            amount.text = "1000000"
+            currency.text ="€"
+        }
+    }
 }
